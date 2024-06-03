1043b1efeacea6489ca1451e4f3b1191c54220e6
ZeroPlaya <kurtduncanbustillos@gmail.com>
2024-06-03 08:20:47 +0800

    Implemented deliveryView w/ switching

--- Controller/__init__.py
+++ Controller/__init__.py
@@ -3,5 +3,6 @@ from Controller.loginController import *
 from Controller.dashboardController import *
 from Controller.productController import *
 from Controller.reportController import *
+from Controller.deliveryController import *
 
-__all__ = ["signupController", "loginController", "dashboardController", "productController", "reportController"]
+__all__ = ["signupController", "loginController", "dashboardController", "productController", "reportController", "deliveryController"]
