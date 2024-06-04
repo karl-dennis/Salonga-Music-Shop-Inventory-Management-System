821418e071dfc4e8ebd598fd9414f2794cedd65c
karl-dennis <karlgaspan0412@gmail.com>
2024-06-04 22:26:04 +0800

    boilerplates for models

--- Controller/inventoryController.py
+++ Controller/inventoryController.py
@@ -3,5 +3,8 @@ from View import inventoryView
 class inventoryController:
     def __init__(self):
         pass
-    def main(self):
+    def fetch_data(self):
+        pass
+
+    def update_data(self):
         pass
