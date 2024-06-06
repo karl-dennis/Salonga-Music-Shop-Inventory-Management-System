a55c80e4ca3e7d065541e307ddc8f54e0f269c3d
karl-dennis <karlgaspan0412@gmail.com>
2024-06-06 13:55:16 +0800

    inventory model update

--- Model/inventoryModel.py
+++ Model/inventoryModel.py
@@ -1,12 +1,12 @@
 import sqlite3
 class inventoryModel:
     def __init__(self):
-        self.connectDatabase = None
-        self.cursor = None
+        # self.connectDatabase = None
+        # self.cursor = None
 
         self.createDatabase()
         self.create_product_table()
-
+        self.create_stock_level_table()
     def createDatabase(self):
         try:
             self.connectDatabase = sqlite3.connect('salonga_music_shop.db')
@@ -17,8 +17,19 @@ class inventoryModel:
     def create_product_table(self):
         try:
             create_table_query_products = '''CREATE TABLE IF NOT EXISTS products(
-            product_id INTEGER PRIMARY KEY, product_name TEXT, product_price INTEGER, stock_level INTEGER, category TEXT
+            product_id INTEGER PRIMARY KEY, product_name TEXT, product_price INTEGER, category TEXT PRIMARY KEY,
+            date TEXT
             )'''
             self.cursor.execute(create_table_query_products)
+        except sqlite3.Error as e:
+            print('Error:', e)
+
+    def create_stock_level_table(self):
+        try:
+            create_table_query_stocks = '''CREATE TABLE IF NOT EXISTS stocks(
+                        category TEXT, stock_level INTEGER,
+                        FOREIGN KEY(category) REFERENCES products(category)
+                        )'''
+            self.cursor.execute(create_table_query_stocks)
         except sqlite3.Error as e:
             print('Error:', e)
