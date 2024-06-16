d2a01b5b9fef4eefa1cbe99db233d23af45809e1
ZeroPlaya <kurtduncanbustillos@gmail.com>
2024-06-16 10:53:15 +0800

    Implemented registration form frames

--- View/productView.py
+++ View/productView.py
@@ -30,7 +30,38 @@ class productView(ctk.CTkFrame):
         self.productRegFrame = ctk.CTkFrame(self.productFrame, width=209, height=594, fg_color='#F7F7F7', corner_radius=7)
         self.productRegFrame.place(x=12, y=10)
         
+        self.imageFrame = ctk.CTkFrame(self.productRegFrame, width=128, height=128)
+        self.imageFrame.place(x=40  , y=44)
         
+        self.nameFrame = ctk.CTkFrame(self.productRegFrame, width=160, height=56)
+        self.nameFrame.place(x=24, y=186)
+        
+        self.brandFrame = ctk.CTkFrame(self.productRegFrame, width=160, height=56)
+        self.brandFrame.place(x=24, y=250)
+        
+        self.typeFrame = ctk.CTkFrame(self.productRegFrame, width=160, height=56)
+        self.typeFrame.place(x=24, y=314)
+        
+        self.quantityFrame = ctk.CTkFrame(self.productRegFrame, width=160, height=56)
+        self.quantityFrame.place(x=24, y=378)
+        
+        self.priceFrame = ctk.CTkFrame(self.productRegFrame, width=160, height=56)
+        self.priceFrame.place(x=24, y=442)
+        
+        self.buttonFrame = ctk.CTkFrame(self.productRegFrame, width=160, height=26, bg_color='transparent', fg_color='transparent')
+        self.buttonFrame.place(x=24, y=532)    
+        
+        self.clearButton = ctk.CTkButton(self.buttonFrame, width=72, height=26, bg_color='transparent', 
+                                         fg_color='#E2E2E2', hover_color='#D5D5D5', corner_radius=7,
+                                         text='Clear', font=('Consolas', 12), text_color='#595959', 
+                                         )
+        self.clearButton.place(x=0, y=0)
+        
+        self.saveButton = ctk.CTkButton(self.buttonFrame, width=72, height=26,  bg_color='transparent',
+                                        fg_color='#1FB2E7', hover_color='#2193BC', corner_radius=7,
+                                        text='Save', font=('Consolas', 12), text_color='#F7F7F7',
+                                        )
+        self.saveButton.place(x=88, y=0)
         
         self.label = ctk.CTkLabel(self.productRegFrame, text="Add Products", font=('Inter Medium', 13))
         self.label.place(x=14, y=7)
